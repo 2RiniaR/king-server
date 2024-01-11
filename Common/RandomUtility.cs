696920b224ea166c23d820ef1ba02f2c8f50b147
watano1168 <watano1168@gmail.com>
2024-01-12 00:06:35 +0900

    RareReply: 人間味を持たせる

---
+++ Common/RandomUtility.cs
@@ -0,0 +1,16 @@
+﻿namespace Approvers.King.Common;
+
+public static class RandomUtility
+{
+    private static readonly Random Random = new();
+
+    public static float GetRandomFloat(float max)
+    {
+        return GetRandomFloat(0f, max);
+    }
+
+    public static float GetRandomFloat(float min, float max)
+    {
+        return (float)(min + (max - min) * Random.NextDouble());
+    }
+}
