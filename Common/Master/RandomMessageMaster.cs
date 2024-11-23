a7f48af8fb8dda634c2655ac2baf31733fcd691f
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2024-11-23 23:42:08 +0900

    Code clean up (#25)

--- Common/Master/RandomMessageMaster.cs
+++ Common/Master/RandomMessageMaster.cs
@@ -15,13 +15,13 @@ public enum RandomMessageType
 public class RandomMessage : MasterRecord<string>
 {
     public override string Key => Id;
-    
+
     [field: MasterStringValue("id")]
     public string Id { get; }
-    
+
     [field: MasterEnumValue("type", typeof(RandomMessageType))]
     public RandomMessageType Type { get; }
-    
+
     [field: MasterStringValue("content")]
     public string Content { get; }
 }
