69fa1aedacf3734a946cb5a8ae48c4515e65c9f4
2riniar <watano1168@gmail.com>
2026-01-25 05:30:44 +0900

    Ichiyo: ユーザー識別機能と時刻情報の追加
    ## 変更内容
    - AIへの入力フォーマットを `[時刻 JST] [ユーザー名]: メッセージ` に変更
    - ユーザー名マスタ（ichiyo_user）を追加し、マスタに登録があればその名前を使用
    - マスタになければDiscordの表示名を使用
    - セッション管理を改善: リプライをメンションより優先して処理
    - 1メッセージにつき1回のみ返答可能に（同時リプライ対策）
    - --json-schemaオプションを削除し、通常のテキスト出力に変更
    
    ## 新規ファイル
    - Common/Master/IchiyoUserMaster.cs
    - Common/Discord/Instances/IchiyoBotInstance.cs
    - Common/Models/IchiyoSessionStore.cs
    - Events/Ichiyo/IchiyoChatPresenter.cs
    - Common/Master/IchiyoSettingMaster.cs

--- Common/Discord/DiscordManager.cs
+++ Common/Discord/DiscordManager.cs
@@ -16,18 +16,23 @@ public class DiscordManager : Singleton<DiscordManager>
     private LoxyBotInstance _loxyBot = null!;
     public static LoxyBotInstance LoxyBot => Instance._loxyBot ??= new LoxyBotInstance();
 
+    private IchiyoBotInstance _ichiyoBot = null!;
+    public static IchiyoBotInstance IchiyoBot => Instance._ichiyoBot;
+
     public static async Task InitializeAsync()
     {
         Instance._issoBot = new IssoBotInstance();
         Instance._eyesBot = new EyesBotInstance();
         Instance._youBot = new YouBotInstance();
         Instance._loxyBot = new LoxyBotInstance();
+        Instance._ichiyoBot = new IchiyoBotInstance();
 
         await Task.WhenAll(
             Instance._issoBot.InitializeAsync(),
             Instance._eyesBot.InitializeAsync(),
             Instance._youBot.InitializeAsync(),
-            Instance._loxyBot.InitializeAsync()
+            Instance._loxyBot.InitializeAsync(),
+            Instance._ichiyoBot.InitializeAsync()
         );
     }
 }
